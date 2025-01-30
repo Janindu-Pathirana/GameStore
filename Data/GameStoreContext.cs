14b870997b39001b439fd896f49e5bc60a074d93
Janindu-Pathirana <78893460+Janindu-Pathirana@users.noreply.github.com>
2025-01-30 22:48:57 +0530

    Add connection string for GameStore and implement GameStoreContext with DbSet properties for Game and Genre

--- Data/GameStoreContext.cs
+++ Data/GameStoreContext.cs
@@ -3,4 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Data;
 
-public class GameStoreContext : DbContext { }
+public class GameStoreContext(DbContextOptions<GameStoreContext> options) : DbContext(options)
+{
+    public DbSet<Entities.Game> Game => Set<Entities.Game>();
+    public DbSet<Entities.Genre> Genre => Set<Entities.Genre>();
+}
---
+++ Migrations/20250130171829_InIt.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class InIt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genre",
+                columns: table => new
+                {
+                    GenreId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genre", x => x.GenreId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Game",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    GeneraId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Publisher = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    ReleaseDate = table.Column<DateOnly>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Game", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Game_Genre_GeneraId",
+                        column: x => x.GeneraId,
+                        principalTable: "Genre",
+                        principalColumn: "GenreId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Game_GeneraId",
+                table: "Game",
+                column: "GeneraId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Game");
+
+            migrationBuilder.DropTable(
+                name: "Genre");
+        }
+    }
+}
